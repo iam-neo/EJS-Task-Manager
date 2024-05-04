32c84f5c19a025a05ebf9eb5f3e2ba9feb69e61e
iam-neo <nirmalrokamagar54@gmail.com>
2024-05-04 13:32:56 +0545

    show rout created

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require(`path`);
 const fs = require('fs');
+const { log } = require('console');
 // const { Console } = require('console');
 
 app.set("view engine", "ejs");
@@ -16,6 +17,12 @@ app.get('/', function(req, res) {
         res.render("index", {files: files});
     })
 })
+
+app.get('/file/:filename', function(req, res) {
+    fs.readFile(`./files/${req.params.filename}`,"utf-8" , function(err, filedata){
+        res.render('show', {filename: req.params.filename, filedata: filedata});
+    })
+    })
 
 app.post('/create', function(req, res) {
     fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
